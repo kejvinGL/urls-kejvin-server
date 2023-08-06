5130e22fa0eb1080b6e5a9c3b834f4f7e4a83c38
kejvinGL <kejvinbraka@gmail.com>
2023-08-06 21:40:07 +0200

    fixed crash

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,31 @@ const router = express.Router();
 const Url = require('../models/Url')
 
 router.get('/:code', async (req, res) => {
-    const url = await Url.findOne({ urlCode: req.params.code })
-    res.redirect(url.longUrl)
+    try {
+        const url = await Url.findOne({ urlCode: req.params.code });
+        if (url) {
+            res.redirect(url.longUrl);
+        } else {
+            res.status(404).send('URL not found.');
+        }
+    } catch (err) {
+        res.status(500).send('Internal Server Error.');
+    }
 });
+
 router.delete('/:code', async (req, res) => {
-    const url = await Url.findOneAndDelete({ urlCode: req.params.code })
-    res.json(url)
-})
+    try {
+        const url = await Url.findOneAndDelete({ urlCode: req.params.code });
+
+        if (url) {
+            res.json(url);
+        } else {
+            res.status(404).json({ message: 'URL not found.' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Server error.' });
+    }
+});
 
 
 module.exports = router;
